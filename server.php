fc27387bc726e98965445e4b2089a700054d65db
banbanxdl/ <38570833@qq.com>
2018-01-24 12:59:54 +0800

    Revert "Update server.php"
    This reverts commit 3d5ec941cf4dcce93d2d93f859744cc716ae4b1c.

--- server.php
+++ server.php
@@ -1,5 +1,9 @@
 <?php
+<<<<<<< HEAD
 
+=======
+//define('APP_PATH', __DIR__ . '/application/');
+>>>>>>> parent of 3d5ec94... Update server.php
 define('APP_PATH','./Application/');
 //define('BIND_MODULE', 'Home');
 define('BIND_MODULE','push/Worker');
