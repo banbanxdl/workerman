c5dbc1dd46f55f336118498255f6ece91b0b0d98
banbanxdl/ <38570833@qq.com>
2018-01-24 11:34:59 +0800

    reback

---
+++ server.php
@@ -0,0 +1,7 @@
+<?php
+//define('APP_PATH', __DIR__ . '/application/');
+define('APP_PATH','./Application/');
+//define('BIND_MODULE', 'Home');
+define('BIND_MODULE','push/Worker');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';
