ea87a024d3df78ab7e966a7bf1cbfe0cef87bec6
banbanxdl/ <38570833@qq.com>
2018-01-24 12:01:10 +0800

    1.245

--- server.php
+++ server.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
 
-=======
-//define('APP_PATH', __DIR__ . '/application/');test
->>>>>>> 3d5ec941cf4dcce93d2d93f859744cc716ae4b1c
 define('APP_PATH','./Application/');
 //define('BIND_MODULE', 'Home');
 define('BIND_MODULE','push/Worker');
